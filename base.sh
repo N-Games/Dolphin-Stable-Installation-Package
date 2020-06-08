ecca64d937ceb027597fea64163432fb1239b3cb
N-Games <58544833+N-Games@users.noreply.github.com>
2020-06-08 15:17:10 +0200

    Create base.sh

---
+++ base.sh
@@ -0,0 +1,24 @@
+echo "welcome to N-Games' Dolphin installation program, now you don't need to write 8 commands"
+echo
+echo "Enter a number from the choices below and then press ENTER to configure the specified program
+echo "1..............Full Dolphin installation with the required packages
+echo "2..............Installing the required packages for Dolphin
+echo "3..............Just for installing Dolphin
+echo
+echo
+read -p "Make a selection: " userInput
+
+echo "you have chosen $userInput"
+if [[ $userInput == 1 ]]; then
+  echo "This is where the initial setup script will be!"
+  #  bash -c "$(curl -s https://raw.githubusercontent.com/N-Games/Dolphin-Stable-Installation-Package/master/main.sh)"
+  
+  echo "you have chosen $userInput"
+if [[ $userInput == 2 ]]; then
+  echo "This is where the initial setup script will be!"
+  #  bash -c "$(curl -s https://raw.githubusercontent.com/N-Games/Dolphin-Stable-Installation-Package/master/main.sh)"
+  echo "you have chosen $userInput"
+  
+if [[ $userInput == 3 ]]; then
+  echo "This is where the initial setup script will be!"
+  #  bash -c "$(curl -s https://raw.githubusercontent.com/N-Games/Dolphin-Stable-Installation-Package/master/main.sh)"
