b8459d9abbd1f2eb293111603fda1900eccaa33b
N-Games <58544833+N-Games@users.noreply.github.com>
2020-06-08 15:15:12 +0200

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,14 @@
+echo "installing the stable build of Dolphin with the other requirment extensions UwU"
+sudo apt install git
+sudo apt install cmake
+sudo apt install pkg-config
+sudo apt install gcc
+sudo apt install libwxbase3.0-dev
+sudo apt install libwxgtk3.0-dev
+sudo apt install libgtk2.0-dev
+sudo apt install libxext-dev
+sudo apt install libreadline-dev
+sudo apt install libgl1-mesa-dev
+sudo apt install libevdev-dev
+sudo apt install libudev-dev
+sudo apt install dolphin-emu
