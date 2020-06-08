2c844f1980398b20f40aabd295fc11fb25f66e84
N-Games <58544833+N-Games@users.noreply.github.com>
2020-06-08 15:16:04 +0200

    Create pack.sh

---
+++ pack.sh
@@ -0,0 +1,13 @@
+echo "installing the requirment extensions for Dolphin UwU"
+sudo apt install git
+sudo apt install cmake
+sudo apt install pkg-config
+sudo apt install gcc
+sudo apt install libwxbase3.0-dev
+sudo apt install libwxgtk3.0-dev
+sudo apt install libgtk2.0-dev
+sudo apt install libxext-dev
+sudo apt install libreadline-dev
+sudo apt install libgl1-mesa-dev
+sudo apt install libevdev-dev
+sudo apt install libudev-dev
